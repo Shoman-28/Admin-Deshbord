85bdd980aac3175378e928362f3badca34322f02
shoman <northstar9928@gmail.com>
2022-05-31 11:44:03 +0600

    Add from

--- src/Components/AddProducts/AddFrom.jsx
+++ src/Components/AddProducts/AddFrom.jsx
@@ -8,23 +8,56 @@ const AddFrom = () => {
 
     console.log(watch("example"));
     return (
-        <div className='addProduct-from'>
+        <div className='addProduct-from w-90'>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="procuct-name">
-                    <label htmlFor="fasf">Product Nmae</label><br/>
-                    {errors.name && <span style={{color:"red",fontSize:'13px'}}>Plasse enter Product name</span>}
-                <input name="name" {...register("name", { required: true })} />
-                
-                </div>
-                <div className="procuct-price">
-                    <label htmlFor="">Price</label><br/>
-                    {errors.price && <span style={{color:"red",fontSize:'13px'}}>Enter Product Price</span>}
-                <input price="price"{...register("price", { required: true })} />
-                
-                </div>
-                
+                <div className="">
+                    <div className="procuct-name d-flex justify-content-between">
+                        <label>Product Nmae {errors.name && <span style={{ color: "red", fontSize: '13px' }}>*</span>}</label>
 
-                <input type="submit" />
+                        <input type="text" name="name" {...register("name", { required: true })} className="addInputfrom" />
+
+                    </div>
+                    <div className="procuct-price d-flex justify-content-between">
+                        <div className="">
+                            <label >Price  {errors.price && <span style={{ color: "red", fontSize: '13px' }}>*</span>}</label>
+                        </div>
+                        <div className="">
+                            <input type="number" price="price"{...register("price", { required: true })} className="addInputfrom" />
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                        <div className="">
+                            <label>Brand Name
+                                {errors.brandName && <span style={{ color: "red", fontSize: '13px' }}>*</span>}
+                            </label>
+                        </div>
+                        <div className="">
+
+                            <input type="text" brandName="brandName"{...register("brandName", { required: true })} className="addInputfrom" />
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                        <div className="">
+                            <label >Image of Product
+                                {errors.productImage && <span style={{ color: "red", fontSize: '13px' }}>*</span>}
+                            </label>
+                        </div>
+                        <div className="">
+                            <input type="file" className="product-image addInputfrom" productImage="productImage"{...register("productImage", { required: true })} />
+                        </div>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                        <div className="">
+                            <label className="">Additional Description
+                                {errors.productDetails && <span style={{ color: "red", fontSize: '13px' }}>*</span>}
+                            </label>
+                        </div>
+                        <div className="">
+                            <textarea className="addInputfrom addDetailsfrom" placeholder="product Details" type="text" productDetails="productDetails" {...register("productDetails", { required: true })} /><br />
+                            <input type="submit" className="submit" />
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     );
